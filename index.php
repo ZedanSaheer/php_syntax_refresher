7ca17ae50039e51c02b132501e7f4a4858efff2b
ZedanSaheer <zed.saheer5@gmail.com>
2024-06-27 11:04:40 +0300

    Testing out PHP

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+echo 'Hello, Welcome to php'; 
+
+echo 123,'Hello';
+
+print_r([1,2,3]);
+
+var_dump('Hello');
+
+var_export('Hello');
+
+// Option closing unless there is some HTML below
+?> 
