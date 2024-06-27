29d56804fe2aa60f80a1fcc89b6a2eb182cc83ea
ZedanSaheer <zed.saheer5@gmail.com>
2024-06-27 13:54:34 +0300

    Understanding variables

---
+++ variables.php
@@ -0,0 +1,21 @@
+<?php
+// Strings,Integers,Floats,Bool,Array,Object,NULL,Resource
+
+// $name_of_variables - $ sign followed by the naming convention, Cannot start with numbers and is case-sensitive
+
+$isComponentTrue = true;
+$DB_Plug = 100.22;
+$CompanyName = 'Multixion LLC';
+
+echo "$CompanyName is the parent company of NoNamePOS";
+
+// Concatenation is . instead of +
+
+echo $CompanyName . ' is the parent company of NoNamePOS ';
+
+// Define = Constant
+define('HOST','localhost');
+define('DB_NAME','dev_db');
+
+
+
